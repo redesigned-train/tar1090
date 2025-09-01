5d952996aa94581c54f101308c97a632fd240f1e
Maria Merkel <maria@merkel.name>
2025-09-01 16:01:26 +0200

    Fixed Mapbox Satellite

--- html/layers.js
+++ html/layers.js
@@ -341,7 +341,7 @@ function createBaseLayers() {
             },
         }));
         world.push(new ol.mapboxStyle.MapboxVectorLayer({
-            styleUrl: 'apbox://styles/mapbox/satellite-v7',
+            styleUrl: 'mapbox://styles/mapbox/satellite-v9',
             accessToken: MapboxAPIKey,
             properties: {
                 name: 'mapbox_satellite',
