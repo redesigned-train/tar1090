658ccde8e54145377881e92c42ad87efeb9ab016
Maria Merkel <maria@merkel.name>
2025-09-05 11:32:18 +0200

    test

--- html/script.js
+++ html/script.js
@@ -3440,7 +3440,7 @@ function refreshSelected() {
 
     jQuery('#selected_callsign').html('<a href="https://flightradar24.com/' + selected.name + '" target="_blank" style="color: inherit">' + selected.name + '</a>');
 
-    jQuery('#selected_flight_number').html = "<span style='font-family: monospace;' class=identSmall>Flight Number:" + NBSP + selected.flight_number + "</span>"
+    jQuery('#selected_flight_number').html("<span style='font-family: monospace;' class=identSmall>Flight Number:" + NBSP + selected.flight_number + "</span>");
 
     if (showTrace) {
         if (selected.position_time) {
